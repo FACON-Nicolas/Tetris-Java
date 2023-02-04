8b115ef18d7b629c675783d03a9076ea6e53b852
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-04 12:25:53 +0000

    add doc for cell

--- src/faconnicolas/project/tetris/grid/ICell.java
+++ src/faconnicolas/project/tetris/grid/ICell.java
@@ -3,10 +3,24 @@ package faconnicolas.project.tetris.grid;
 import faconnicolas.project.tetris.window.Positionable;
 import faconnicolas.project.tetris.color.Colorable;
 
+
+/**
+ * ICell Interface, extends Positionable and Colorable interfaces
+ */
 public interface ICell extends Positionable, Colorable {
 
+    /**
+     * get row
+     *
+     * @return row
+     */
     int getRow();
 
+    /**
+     * get column
+     *
+     * @return column
+     */
     int getColumn();
 
 }
