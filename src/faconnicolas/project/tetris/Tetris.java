484c747d7038e0d9072eddfebde707b300a1913e
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-04 15:00:09 +0000

    replace Hello World by game launch

--- src/faconnicolas/project/tetris/Tetris.java
+++ src/faconnicolas/project/tetris/Tetris.java
@@ -1,7 +1,15 @@
 package faconnicolas.project.tetris;
 
-public class Main {
+import faconnicolas.project.tetris.window.Window;
+
+import javax.swing.*;
+
+public class Tetris {
+
+    public static final int WIDTH = 1600;
+    public static final int HEIGHT = 915;
+    public static final String NAME = "Tetris";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> new Window(NAME, WIDTH, HEIGHT));
     }
 }
