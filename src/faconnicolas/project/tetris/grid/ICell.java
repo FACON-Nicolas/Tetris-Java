fa44f5cf1576b15d0e659199be2179ce36979b45
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-05 19:16:38 +0000

    add inheritance in ICell

--- src/faconnicolas/project/tetris/grid/ICell.java
+++ src/faconnicolas/project/tetris/grid/ICell.java
@@ -7,20 +7,4 @@ import faconnicolas.project.tetris.color.Colorable;
 /**
  * ICell Interface, extends Positionable and Colorable interfaces
  */
-public interface ICell extends Positionable, Colorable {
-
-    /**
-     * get row
-     *
-     * @return row
-     */
-    int getRow();
-
-    /**
-     * get column
-     *
-     * @return column
-     */
-    int getColumn();
-
-}
+public interface ICell extends Positionable, Colorable, GridPositionable {}
