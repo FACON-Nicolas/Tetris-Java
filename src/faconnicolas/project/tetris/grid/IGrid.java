8c577394d238b5e5ce26fe692389079cd27279c4
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-05 19:20:39 +0000

    add interface for grid

---
+++ src/faconnicolas/project/tetris/grid/IGrid.java
@@ -0,0 +1,9 @@
+package faconnicolas.project.tetris.grid;
+
+import faconnicolas.project.tetris.window.Drawable;
+
+public interface IGrid extends Gettable, Settable, Drawable {
+
+    void initGrid();
+
+}
