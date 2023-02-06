977a50b1b654924693532e955ee0b7d254ab0f40
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-06 09:14:37 +0000

    add doc in merger

--- src/faconnicolas/project/tetris/grid/GridTetriminosMerger.java
+++ src/faconnicolas/project/tetris/grid/GridTetriminosMerger.java
@@ -1,41 +1,56 @@
 package faconnicolas.project.tetris.grid;
 
+/**
+ * Decorator used to merge tetriminos with the grid.
+ */
 public class GridTetriminosMerger extends GridDecorator {
 
+    /**
+     * init the decorator
+     *
+     * @param grid decore
+     */
     public GridTetriminosMerger(IGrid grid) {
         super(grid);
     }
 
+    /**
+     * check if it is possible to merge the grid with the tetriminos
+     *
+     * @param tetriminos the tetriminos to "merge"
+     *
+     * @return <code>true</code> if it is possible else <code>false</code>.
+     */
     public boolean canMarge(Tetriminos tetriminos) {
         int row = tetriminos.getRow();
         int column = tetriminos.getColumn();
         if (column >= Grid.GRID_WIDTH - (1+ tetriminos.length()) || row >= Grid.GRID_HEIGHT - 1) return false;
-        for (int i = row; i > 1 || (tetriminos.getRow() - i) < tetriminos.length(); i--) {
-            for (int j = column; j - tetriminos.getColumn() < tetriminos.length(); j++) {
-                if (tetriminos.get(tetriminos.getRow() - i, j - column) != 0 && grid.get(i, j) != 0)
+        for (int i = row; i > 1 || (tetriminos.getRow() - i) < tetriminos.length(); i--)
+            for (int j = column; j - tetriminos.getColumn() < tetriminos.length(); j++)
+                if (tetriminos.get(row - i, j - column) != 0 && get(i, j) != 0)
                     return false;
-            }
-        }
         return true;
     }
 
+    /**
+     * merge tetriminos with grid
+     *
+     * @param tetriminos tetriminos to merge
+     */
     public void merge(Tetriminos tetriminos) {
         if (!canMarge(tetriminos)) return;
         int row = tetriminos.getRow();
         int column = tetriminos.getColumn();
-        for (int i = row; i > 1 || (row - i) < tetriminos.length(); i--) {
-            for (int j = column; j - column < tetriminos.length(); j++) {
+        for (int i = row; i > 1 || (row - i) < tetriminos.length(); i--)
+            for (int j = column; j - column < tetriminos.length(); j++)
                 set(i, j, tetriminos.get(row - i, j - column));
-            }
-        }
-
-        for (int i = 0; i < Grid.GRID_HEIGHT; i++) {
-            for (int j = 0; j < Grid.GRID_WIDTH; j++)
-                System.out.print(get(i, j) + " ");
-            System.out.println();
-        }
     }
 
+    /**
+     * erase tetriminos from the grid.
+     *
+     * @param tetriminos tetriminos to erase
+     */
     public void erase(Tetriminos tetriminos) {
         int row = tetriminos.getRow();
         int column = tetriminos.getColumn();
@@ -44,7 +59,7 @@ public class GridTetriminosMerger extends GridDecorator {
             for (int j = column; j - column < tetriminos.length(); j++) {
                 int r = row - i; // r is for row
                 int c = j - column; // c is for column
-                if (grid.get(i, j) != 0 && tetriminos.get(r, c) != 0) grid.set(i, j, 0);
+                if (get(i, j) != 0 && tetriminos.get(r, c) != 0) set(i, j, 0);
             }
         }
     }
