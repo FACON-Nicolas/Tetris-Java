2e2089f5cc98320a630327e1f10a3e6799403c2c
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-06 09:17:39 +0000

    add doc main class'

--- src/faconnicolas/project/tetris/Tetris.java
+++ src/faconnicolas/project/tetris/Tetris.java
@@ -4,11 +4,31 @@ import faconnicolas.project.tetris.window.Window;
 
 import javax.swing.*;
 
+/**
+ * main class, used to launch the game
+ */
 public class Tetris {
 
+    /**
+     * width
+     */
     public static final int WIDTH = 1600;
+
+    /**
+     * height
+     */
     public static final int HEIGHT = 915;
+
+    /**
+     * window name
+     */
     public static final String NAME = "Tetris";
+
+    /**
+     * main method.
+     *
+     * @param args no args.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Window(NAME, WIDTH, HEIGHT));
     }
