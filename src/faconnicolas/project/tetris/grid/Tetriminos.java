1eea9de4e4a5e69991338bef6a4bbc935ccb94d1
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-06 09:15:46 +0000

    add doc in Tetriminos

--- src/faconnicolas/project/tetris/grid/Tetriminos.java
+++ src/faconnicolas/project/tetris/grid/Tetriminos.java
@@ -1,22 +1,43 @@
 package faconnicolas.project.tetris.grid;
 
-import java.util.Random;
-
+/**
+ * Tetriminos class, used to implement tetriminos. (obviously)
+ */
 public class Tetriminos implements ITetriminos, GridPositionable {
 
-    private int[][] tetriminos;
+    /**
+     * tetriminos array
+     */
+    private int[][] tetriminosArray;
 
-    private int row, column;
+    /**
+     * row attribute
+     */
+    private int row;
 
+    /**
+     * Column attribute
+     */
+    private int column;
+
+    /**
+     * Tetriminos constructor, init the tetriminos according to a string value.
+     *
+     * @param value the string value.
+     */
     public Tetriminos(String value) {
         initTetriminos(value);
         row = 0;
-        //column = new Random().nextInt(0, Grid.GRID_WIDTH-length());
         column = 5;
     }
 
+    /**
+     * init tetriminos, called in the constructor.
+     *
+     * @param value the value to get the tetriminos.
+     */
     private void initTetriminos(String value) {
-        tetriminos = switch(value.toUpperCase()) {
+        tetriminosArray = switch(value.toUpperCase()) {
             case "T" -> TetriminosFactory.getT();
             case "S" -> TetriminosFactory.getS();
             case "Z" -> TetriminosFactory.getZ();
@@ -39,37 +60,72 @@ public class Tetriminos implements ITetriminos, GridPositionable {
      */
     @Override
     public void rotate() {
-        int[][] copy = new int[tetriminos.length][tetriminos.length];
+        int[][] copy = new int[tetriminosArray.length][tetriminosArray.length];
         for (int i = 0; i < length(); i++)
             for (int j = 0; j < length(); j++)
-                copy[j][length() - (i+1)] = tetriminos[i][j];
-        tetriminos = copy;
+                copy[j][length() - (i+1)] = tetriminosArray[i][j];
+        tetriminosArray = copy;
     }
 
+
+    /**
+     * get the length
+     *
+     * @return the length
+     */
     @Override
     public int length() {
-        return tetriminos.length;
+        return tetriminosArray.length;
     }
 
+    /**
+     * Gets value at a specific place.
+     * <p>
+     * @param row the row to check
+     *
+     * @param col the column to check
+     *
+     * @return the value
+     */
     public int get(int row, int col) {
-        return tetriminos[row][col];
+        return tetriminosArray[row][col];
     }
 
+    /**
+     * get current row
+     *
+     * @return current row
+     */
     @Override
     public int getRow() {
         return row;
     }
 
+    /**
+     * get current column
+     *
+     * @return current column
+     */
     @Override
     public int getColumn() {
         return column;
     }
 
+    /**
+     * set current row
+     *
+     * @param row new row
+     */
     @Override
     public void setRow(int row) {
         this.row = row;
     }
 
+    /**
+     * set current column
+     *
+     * @param column new column
+     */
     @Override
     public void setColumn(int column) {
         this.column = column;
