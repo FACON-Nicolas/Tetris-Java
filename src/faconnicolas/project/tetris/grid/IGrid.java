f061fc15e63cb42ab09c03830d122e552b550961
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-06 09:14:51 +0000

    add doc in IGrid

--- src/faconnicolas/project/tetris/grid/IGrid.java
+++ src/faconnicolas/project/tetris/grid/IGrid.java
@@ -2,8 +2,14 @@ package faconnicolas.project.tetris.grid;
 
 import faconnicolas.project.tetris.window.Drawable;
 
+/**
+ * IGrid is an interface to implements the grids.
+ */
 public interface IGrid extends Gettable, Settable, Drawable {
 
+    /**
+     * init the grid.
+     */
     void initGrid();
 
 }
