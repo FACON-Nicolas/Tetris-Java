b0e1bafc05f0052b3cf2fcdd84a7bee6c291f7c7
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-06 09:16:31 +0000

    add doc in panel

--- src/faconnicolas/project/tetris/window/Panel.java
+++ src/faconnicolas/project/tetris/window/Panel.java
@@ -2,22 +2,35 @@ package faconnicolas.project.tetris.window;
 
 import faconnicolas.project.tetris.grid.Grid;
 import faconnicolas.project.tetris.grid.GridTetriminosMerger;
-import faconnicolas.project.tetris.grid.IGrid;
-import faconnicolas.project.tetris.grid.Tetriminos;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Panel is the controller in the game.
+ */
 public class Panel extends JPanel implements ActionListener, Updatable {
 
+    /**
+     * game grid
+     */
     private final GridTetriminosMerger grid;
 
+    /**
+     * timer for each frame.
+     */
     private final Timer timer;
 
+    /**
+     * delay between each frame (in millis)
+     */
     public static final int DELAY = 16;
 
+    /**
+     * panel constructor, init panel and grid.
+     */
     public Panel() {
         super();
         setFocusable(true);
@@ -26,11 +39,21 @@ public class Panel extends JPanel implements ActionListener, Updatable {
         grid = new GridTetriminosMerger(new Grid());
     }
 
+    /**
+     * called at each frame
+     *
+     * @param actionEvent none
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         update();
     }
 
+    /**
+     * paint every component in the game.
+     *
+     * @param g graphics
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
