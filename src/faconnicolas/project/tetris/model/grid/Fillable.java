ef5cac2f6d3c19c7f848bf2e2797b4f557ac63e7
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-14 10:40:35 +0000

    add Fillable interface

---
+++ src/faconnicolas/project/tetris/model/grid/Fillable.java
@@ -0,0 +1,11 @@
+package faconnicolas.project.tetris.model.grid;
+
+public interface Fillable {
+
+    /**
+     * Check if the container is full
+     *
+     * @return <code>true</code> if it is full else <code>false</code>
+     */
+    boolean isFull();
+}
