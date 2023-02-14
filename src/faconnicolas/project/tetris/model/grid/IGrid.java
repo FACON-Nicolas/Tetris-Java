b6b2f463aeb2ccd05176f7af614a771f6e50c944
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2023-02-14 12:51:25 +0000

    extends updatable

--- src/faconnicolas/project/tetris/model/grid/IGrid.java
+++ src/faconnicolas/project/tetris/model/grid/IGrid.java
@@ -1,15 +1,24 @@
 package faconnicolas.project.tetris.model.grid;
 
+import faconnicolas.project.tetris.model.tetriminos.Tetriminos;
+import faconnicolas.project.tetris.model.window.Updatable;
 import faconnicolas.project.tetris.view.Drawable;
 
 /**
  * IGrid is an interface to implements the grids.
  */
-public interface IGrid extends Gettable, Settable, Drawable, Fillable {
+public interface IGrid extends Gettable, Settable, Drawable, Fillable, Updatable {
 
     /**
      * init the grid.
      */
     void initGrid();
+
+    /**
+     * set the grid tetriminos
+     *
+     * @param tetriminos new tetriminos
+     */
+    void setTetriminos(Tetriminos tetriminos);
 
 }
